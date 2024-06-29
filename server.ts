e58c1844b1f06f10272a30b4b0693c6a8863d84f
james <1397213571@qq.com>
2024-06-29 17:45:11 +0800

    add basic config

---
+++ server.ts
@@ -0,0 +1,24 @@
+import express, { Application } from 'express';
+import { AddressInfo } from 'net';
+import { getAllCourses } from './server/get-courses.route';
+import { saveCourse } from './server/save-course.route';
+
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+const app: Application = express();
+
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
+
+app.route('/api/courses').get(getAllCourses);
+
+app.route('/api/courses/:id').put(saveCourse);
+
+const httpServer = app.listen(9000, () => {
+  console.log(
+    'HTTP REST API Server running at http://localhost:' +
+      (httpServer.address() as AddressInfo).port
+  );
+});

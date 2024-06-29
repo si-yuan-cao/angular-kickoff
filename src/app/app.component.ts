3281c486c9a7e52206acba7a170d2b94738a016d
james <1397213571@qq.com>
2024-06-29 18:01:58 +0800

    fix basic config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,6 @@ import { COURSES } from '../db-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}

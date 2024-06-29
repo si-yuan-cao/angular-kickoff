42fe25592f45becf3d6f92ed887d7a5ba034b943
james <1397213571@qq.com>
2024-06-29 20:23:27 +0800

    Building our First Angular Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseCardComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrl: './course-card.component.scss',
+})
+export class CourseCardComponent {}

926dd3290f3efddb11b330812f8c288f45f9d14c
james <1397213571@qq.com>
2024-06-30 17:03:40 +0800

    The AfterViewInit Lifecycle Hook - How deep can we query with View Child?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -8,13 +8,23 @@ import { CourseCardComponent } from './course-card/course-card.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   courses: Course[] = [...COURSES];
   course = COURSES[0];
 
   @ViewChild('cardRef1', { read: ElementRef }) card1?: ElementRef;
   @ViewChild('cardRef2') card2?: CourseCardComponent;
   @ViewChild('container') containerDiv?: ElementRef;
+  @ViewChild('courseImage') courseImage?: ElementRef;
+
+  constructor() {
+    console.log('containerDiv', this.containerDiv);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('containerDiv -- ngAfterViewInit', this.containerDiv);
+    console.log('courseImage -- ngAfterViewInit', this.courseImage);
+  }
 
   onCourseSelected(course: Course) {
     console.log('card1', this.card1);

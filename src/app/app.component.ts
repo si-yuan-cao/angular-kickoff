6aaedcae9acaa8d74ce23ebeba269cfe419936e0
james <1397213571@qq.com>
2024-06-30 16:22:01 +0800

    Angular Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ import { Course } from './model/course';
 })
 export class AppComponent {
   courses: Course[] = [...COURSES];
+  title = COURSES[0].description;
+  price = 9.99797956675;
+  rate = 0.67;
+  startDate = new Date(2000, 0, 1);
+  course = COURSES[0];
 
   onCourseSelected(course: Course) {
     console.log('Card was clicked', course);

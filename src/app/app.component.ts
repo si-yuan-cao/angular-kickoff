0ca405d5decb339bbd83c2e08ccdb2966ed17c5c
james <1397213571@qq.com>
2024-06-30 18:22:30 +0800

    basic config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { COURSES } from '../db-data';
+import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  courses = COURSES;
+
+  @ViewChildren(CourseCardComponent, { read: ElementRef })
+  cards?: QueryList<ElementRef>;
+
+  constructor() {}
+
+  ngAfterViewInit() {}
+
+  onCourseSelected(course: Course) {}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { CourseImageComponent } from './course-image/course-image.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CourseCardComponent, CourseImageComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
